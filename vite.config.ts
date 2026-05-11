c86d9fb768f0f91c2ba058038484e9abbf037fb6
Diana Balconi <dalebv87@gmail.com>
2026-05-10 20:24:13 -0600

    Restore Tailwind and TSConfig paths plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     react(),
