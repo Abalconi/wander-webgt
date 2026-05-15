e859e0259e3e6ad7eba0522c4f39f1c84e7bb213
Diana Balconi <dalebv87@gmail.com>
2026-05-15 14:26:54 -0600

    fix: avoid template literals in wrapper worker for Cloudflare compatibility

--- scripts/postbuild.cjs
+++ scripts/postbuild.cjs
@@ -32,174 +32,158 @@ if (fs.existsSync(serverAssets)) {
 }
 
 // Create wrapper _worker.js that intercepts sitemap and robots
+// Note: Avoiding template literals (backticks) in the generated file for compatibility
 const wrapper = `import originalWorker from "./_worker_original.js";
 
-const SITEMAP = \\\`<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml">
-  <!-- Home -->
-  <url>
-    <loc>https://wandergt.com/</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en"/>
-  </url>
+var SITEMAP = '<?xml version="1.0" encoding="UTF-8"?>' +
+'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" ' +
+'        xmlns:xhtml="http://www.w3.org/1999/xhtml">' +
+'  <!-- Home -->' +
+'  <url>' +
+'    <loc>https://wandergt.com/</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en"/>' +
+'  </url>' +
+'  <!-- Destinos Index -->' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos"/>' +
+'  </url>' +
+'  <!-- Ofertas -->' +
+'  <url>' +
+'    <loc>https://wandergt.com/ofertas</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/ofertas"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/ofertas"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/ofertas</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/ofertas"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/ofertas"/>' +
+'  </url>' +
+'  <!-- Guía del Viajero -->' +
+'  <url>' +
+'    <loc>https://wandergt.com/guia-viajero</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/guia-viajero"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/guia-viajero"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/guia-viajero</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/guia-viajero"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/guia-viajero"/>' +
+'  </url>' +
+'  <!-- Términos -->' +
+'  <url>' +
+'    <loc>https://wandergt.com/terminos-condiciones</loc>' +
+'  </url>' +
+'  <!-- Destinos Específicos -->' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/punta-cana</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/punta-cana"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/punta-cana"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/punta-cana</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/punta-cana"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/punta-cana"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/cancun</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/cancun"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/cancun"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/cancun</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/cancun"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/cancun"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/curazao</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/curazao"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/curazao"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/curazao</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/curazao"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/curazao"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/aruba</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/aruba"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/aruba"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/aruba</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/aruba"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/aruba"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/rio-de-janeiro</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/rio-de-janeiro"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/rio-de-janeiro"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/rio-de-janeiro</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/rio-de-janeiro"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/rio-de-janeiro"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/cartagena</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/cartagena"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/cartagena"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/cartagena</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/cartagena"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/cartagena"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/mexico-basilica</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/mexico-basilica"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/mexico-basilica"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/mexico-basilica</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/mexico-basilica"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/mexico-basilica"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/isla-mucura</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/isla-mucura"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/isla-mucura"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/isla-mucura</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/isla-mucura"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/isla-mucura"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/destinos/peten</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/peten"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/peten"/>' +
+'  </url>' +
+'  <url>' +
+'    <loc>https://wandergt.com/en/destinos/peten</loc>' +
+'    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/peten"/>' +
+'    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/peten"/>' +
+'  </url>' +
+'</urlset>';
 
-  <!-- Destinos Index -->
-  <url>
-    <loc>https://wandergt.com/destinos</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos"/>
-  </url>
-
-  <!-- Ofertas -->
-  <url>
-    <loc>https://wandergt.com/ofertas</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/ofertas"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/ofertas"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/ofertas</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/ofertas"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/ofertas"/>
-  </url>
-
-  <!-- Guía del Viajero -->
-  <url>
-    <loc>https://wandergt.com/guia-viajero</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/guia-viajero"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/guia-viajero"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/guia-viajero</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/guia-viajero"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/guia-viajero"/>
-  </url>
-
-  <!-- Términos -->
-  <url>
-    <loc>https://wandergt.com/terminos-condiciones</loc>
-  </url>
-
-  <!-- Destinos Específicos -->
-  <url>
-    <loc>https://wandergt.com/destinos/punta-cana</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/punta-cana"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/punta-cana"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/punta-cana</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/punta-cana"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/punta-cana"/>
-  </url>
-
-  <url>
-    <loc>https://wandergt.com/destinos/cancun</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/cancun"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/cancun"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/cancun</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/cancun"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/cancun"/>
-  </url>
-
-  <url>
-    <loc>https://wandergt.com/destinos/curazao</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/curazao"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/curazao"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/curazao</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/curazao"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/curazao"/>
-  </url>
-
-  <url>
-    <loc>https://wandergt.com/destinos/aruba</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/aruba"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/aruba"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/aruba</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/aruba"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/aruba"/>
-  </url>
-
-  <url>
-    <loc>https://wandergt.com/destinos/rio-de-janeiro</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/rio-de-janeiro"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/rio-de-janeiro"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/rio-de-janeiro</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/rio-de-janeiro"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/rio-de-janeiro"/>
-  </url>
-
-  <url>
-    <loc>https://wandergt.com/destinos/cartagena</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/cartagena"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/cartagena"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/cartagena</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/cartagena"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/cartagena"/>
-  </url>
-
-  <url>
-    <loc>https://wandergt.com/destinos/mexico-basilica</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/mexico-basilica"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/mexico-basilica"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/mexico-basilica</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/mexico-basilica"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/mexico-basilica"/>
-  </url>
-
-  <url>
-    <loc>https://wandergt.com/destinos/isla-mucura</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/isla-mucura"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/isla-mucura"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/isla-mucura</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/isla-mucura"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/isla-mucura"/>
-  </url>
-
-  <url>
-    <loc>https://wandergt.com/destinos/peten</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/peten"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/peten"/>
-  </url>
-  <url>
-    <loc>https://wandergt.com/en/destinos/peten</loc>
-    <xhtml:link rel="alternate" hreflang="es" href="https://wandergt.com/destinos/peten"/>
-    <xhtml:link rel="alternate" hreflang="en" href="https://wandergt.com/en/destinos/peten"/>
-  </url>
-</urlset>\\\`;
-
-const ROBOTS = \\\`User-agent: *
-Allow: /
-
-Sitemap: https://wandergt.com/sitemap.xml
-\\\`;
+var ROBOTS = 'User-agent: *\\\\nAllow: /\\\\n\\\\nSitemap: https://wandergt.com/sitemap.xml\\\\n';
 
 export default {
   async fetch(request, env, ctx) {
-    const url = new URL(request.url);
+    var url = new URL(request.url);
     if (url.pathname === "/sitemap.xml") {
       return new Response(SITEMAP, {
         headers: { "Content-Type": "application/xml" },
